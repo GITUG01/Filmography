598a767056bba2ae4796621c90f1b07477df6335
Alexandr <sasha-03-30@mail.ru>
2021-11-01 11:24:06 +0300

    Initial commit

---
+++ app/src/main/java/com/gitug01/filmpgraphy/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gitug01.filmpgraphy.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gitug01.filmpgraphy.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
