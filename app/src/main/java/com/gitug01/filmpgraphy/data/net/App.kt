be47e65842f6dd9c2f8d4da17eea1fdfc51be968
Alexandr <sasha-03-30@mail.ru>
2021-11-16 20:25:19 +0300

    create class App with extension function

---
+++ app/src/main/java/com/gitug01/filmpgraphy/data/net/App.kt
@@ -0,0 +1,13 @@
+package com.gitug01.filmpgraphy.data.net
+
+import android.app.Application
+import android.content.Context
+import com.gitug01.filmpgraphy.domain.repo.DBFilmRepo
+
+class App : Application() {
+    val DBFilmRepo: DBFilmRepo by lazy { WebDBFilmRepoImpl() }
+}
+
+
+val Context.app
+    get() = applicationContext as App
