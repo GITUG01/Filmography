a1ab1a4eda446b3b7cbfc553c764fd4ac366786b
Alexandr <sasha-03-30@mail.ru>
2021-11-19 14:36:03 +0300

    create Database Film Api interface

---
+++ app/src/main/java/com/gitug01/filmpgraphy/data/retrofit/DBFApi.kt
@@ -0,0 +1,13 @@
+package com.gitug01.filmpgraphy.data.retrofit
+
+import com.gitug01.filmpgraphy.data.net.entity.O
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface DBFApi {
+
+    @GET("discover/movie?sort_by=popularity.desc")
+    fun getPopularFilms(@Query("api_key") requestCode: String): Call<O>
+
+}
